21bbffbcc25b3a243f125919c1b2f8cfa7ed19a0
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2019-05-24 11:30:03 +0200

    Vs. 1.0.1

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord   = require("discord.js");
-const auth      = require("./auth.json");
 const client    = new Discord.Client();
+const auth      = require("./auth.json");
+const punycode  = require('punycode');
 
 //own scripts
 const log       = require("./js/log");
@@ -73,7 +74,7 @@ client.on('webhookUpdate', webhookUpdate => {
 });
 
 /**************************************************************************************************************/
-/* GUILD CREATED / LEAVE                                                                                      */ 
+/* GUILD                                                                                                      */ 
 /**************************************************************************************************************/
 
 //Emitted whenever the client joins a guild.
@@ -81,9 +82,88 @@ client.on('guildCreate', async guild => {
 });
 
 //Emitted whenever a guild is deleted/left.
+client.on('guildDelete', async guild => {
+});
 
-client.on("message", async message => {
+/**************************************************************************************************************/
+/* MEMBER                                                                                                     */ 
+/**************************************************************************************************************/
+
+//Emitted whenever a user joins a guild.
+client.on('guildMemberAdd', async member => {
+});
+
+//Emitted whenever a member leaves a guild, or is kicked.
+client.on('guildMemberRemove', async member => {
+});
+
+/**************************************************************************************************************/
+/* REACTIONS                                                                                                  */ 
+/**************************************************************************************************************/
+
+const events = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MESSAGE_REACTION_REMOVE: 'messageReactionRemove'
+};
+
+//https://discordjs.guide/popular-topics/reactions.html#listening-for-reactions-on-old-messages
+//handle raw events (for e.g. uncached messages)
+client.on('raw', async event => {
+	if (!events.hasOwnProperty(event.t)) return;
+  const { d: data } = event;
+  const user = client.users.get(data.user_id);
+  const channel = client.channels.get(data.channel_id) || await user.createDM();
+
+  //if (channel.messages.has(data.message_id)) return; //event trigger 2 times without this
   
+  const message = await channel.fetchMessage(data.message_id);
+  const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;  
+  let reaction = message.reactions.get(emojiKey);
+  
+  if (!reaction) {
+    // Create an object that can be passed through the event like normal
+    const emoji = new Discord.Emoji(client.guilds.get(data.guild_id), data.emoji);
+    reaction = new Discord.MessageReaction(message, emoji, 1, data.user_id === client.user.id);
+  }
+  client.emit(events[event.t], reaction, user, message);
+});
+
+//Emitted whenever a reaction is added to a cached message.
+client.on('messageReactionAdd', async (reaction, user, message)  => {//
+  if(reaction.me === true && message !== undefined){
+    let channelID = message.channel.id;
+    let messageID = message.id;
+    let emoteID   = (reaction.emoji.id !== null) ? reaction.emoji.name + ':' + reaction.emoji.id : punycode.ucs2.decode(reaction.emoji.name);
+
+    let response = await db.reactions(message, 'check', channelID, messageID, emoteID);
+
+    if(response.error == false && response.value !== null && user.bot === false){
+      message.guild.members.get(user.id).addRole(response.value);
+    }
+  }
+});
+
+//Emitted whenever a reaction is removed from a cached message.
+client.on('messageReactionRemove', async (reaction, user, message) => {
+  if(reaction.me === true && message !== undefined){ //bot reacts before
+    let channelID = message.channel.id;
+    let messageID = message.id;
+    let emoteID   = (reaction.emoji.id !== null) ? reaction.emoji.name + ':' + reaction.emoji.id : punycode.ucs2.decode(reaction.emoji.name);
+
+    let response = await db.reactions(message, 'check', channelID, messageID, emoteID);
+
+    if(response.error == false && response.value !== null && user.bot === false){
+      message.guild.members.get(user.id).removeRole(response.value);
+    }
+  }
+});
+
+/**************************************************************************************************************/
+/* MESSAGES                                                                                                   */
+/**************************************************************************************************************/
+
+//Emitted whenever a message is created.
+client.on("message", async message => {
 });
 
 client.login(auth.token);
