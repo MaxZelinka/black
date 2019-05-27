c9483f7cd9f106359c349cf860ea0adc7906999c
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2019-05-27 18:22:55 +0200

    Update messagehandler.js

--- js/messagehandler.js
+++ js/messagehandler.js
@@ -7,62 +7,61 @@ const msg_send = require("./msg_send");
 let config = '';
 
 exports.handler = async (client, message) => {
-    if(message.guild !== null) {
+    if (message.guild !== null) {
         config = (config === '') ? await db.get_config(message.guild) : config;
 
-        if(config !== undefined){
-            const cf_channel = (config[0].Channel !== null) ? config[0].Channel : '';
-            const cf_blacklist = (config[0].blacklist !== null) ? config[0].blacklist : '';
-            const cf_prefix = config[0].Prefix;
-        
-            const args = message.content.trim().split(/ +/g);
-            const command = args.shift().toLowerCase();
-        
-            const regexBlacklist = new RegExp(message.author.id, 'gm');
-            const regexPrefix = new RegExp('^(' + cf_prefix.replace(/\?/gm, '\\?') + ')\\S*', 'gm');
-            const regexChannel = new RegExp(message.channel.id, 'g');
-            
-            if (cf_blacklist.toString().match(regexBlacklist) !== null
-                && message.content.match(regexPrefix) !== null) {
-                //if user is blacklistet & trying to use an command
-                msg_send.embedMessage(client, message.channel.id, 'Blacklist', message.author.toString() + ' - you are blacklistet from using commands.', '#ff0000', 5000);
-            } else {
-                switch(command){
-                    case '?bfirst':
-                        modhandler.first(config, client, message);
+        const cf_channel = (config[0].Channel !== null) ? config[0].Channel : '';
+        const cf_blacklist = (config[0].blacklist !== null) ? config[0].blacklist : '';
+        const cf_prefix = config[0].Prefix;
+
+        const args = message.content.trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+        const regexBlacklist = new RegExp(message.author.id, 'gm');
+        const regexPrefix = new RegExp('^(' + cf_prefix.replace(/\?/gm, '\\?') + ')\\S*', 'gm');
+        const regexChannel = new RegExp(message.channel.id, 'g');
+
+        if (cf_blacklist.toString().match(regexBlacklist) !== null
+            && message.content.match(regexPrefix) !== null) {
+            //if user is blacklistet & trying to use an command
+            msg_send.embedMessage(client, message.channel.id, 'Blacklist', message.author.toString() + ' - you are blacklistet from using commands.', '#ff0000', 5000);
+        } else {
+            switch (command) {
+                case '?bfirst':
+                    modhandler.first(config, client, message);
+                    break;
+                case '?bhelp':
+                    modhandler.help(config, client, message);
+                    break;
+                case cf_prefix + 'help':
+                    modhandler.help(config, client, message);
+                    break;
+                case cf_prefix + 'channel':
+                    modhandler.channel(config, client, message);
+                    break;
+            }
+            if (cf_channel.toString().match(regexChannel) !== null) {
+                //if channel is set
+                switch (command) {
+                    //REACTION
+                    case cf_prefix + 'addrole':
+                        modhandler.addrole(config, client, message);
                         break;
-                    case '?bhelp':
-                        modhandler.help(config, client, message);
+                    case cf_prefix + 'removerole':
+                        modhandler.removerole(config, client, message);
                         break;
-                    case cf_prefix + 'help':
-                        modhandler.help(config, client, message);
+                    case cf_prefix + 'reactionid':
+                        modhandler.reactionid(config, client, message);
                         break;
-                    case cf_prefix + 'channel':
-                        modhandler.channel(config, client, message);
+                    case cf_prefix + 'embedmsg':
+                        modhandler.embedmsg(config, client, message);
                         break;
-                }
-                if(cf_channel.toString().match(regexChannel) !== null){
-                    //if channel is set
-                    switch(command){
-                        //REACTION
-                        case cf_prefix + 'addrole':
-                            modhandler.addrole(config, client, message);
-                            break;
-                        case cf_prefix + 'removerole':
-                            modhandler.removerole(config, client, message);
-                            break;
-                        case cf_prefix + 'reactionid':
-                            modhandler.reactionid(config, client, message);
-                            break;
-                        case cf_prefix + 'embedmsg':
-                            modhandler.embedmsg(config, client, message);
-                            break;
-                        case cf_prefix + 'editmsg':
-                            modhandler.editmsg(config, client, message);
-                            break;
-                    }
+                    case cf_prefix + 'editmsg':
+                        modhandler.editmsg(config, client, message);
+                        break;
                 }
             }
         }
     }
+
 }
