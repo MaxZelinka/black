14d064ca186ac317e6bffebc5f3a24b157806e9e
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2019-06-11 15:44:39 +0200

    delete old log files

--- js/log.js
+++ js/log.js
@@ -1,8 +1,27 @@
-const moment    = require('moment');
-const fs        = require('fs');
+const moment = require('moment');
+const fs = require('fs');
 
-exports.log = function(data){
+exports.log = function (data) {
     fs.appendFile('./logs/' + moment().format('YYYY-MM') + '.log', moment().format('YYYY.MM.DD - HH:mm:ss') + '\t' + data + '\r\n', function (err) {
         if (err) throw err;
+    });
+    del_file();
+}
+
+function del_file() {
+    fs.readdir(dir, (err, files) => {
+        if (err) throw err;
+        files.forEach(file => {
+            if (file.match(/\d{4}-\d{2}.\blog\b/gm) !== null) {
+                const date = file.replace(/.\blog\b/gm, '');
+                const ago = moment().subtract(3, 'month');
+                if (moment(date).isBefore(ago)) {
+                    fs.unlink(dir + file, (err) => {
+                        if (err) throw err;
+                        console.log(dir + file + ' deleted.');
+                    });
+                }
+            }
+        });
     });
 }
