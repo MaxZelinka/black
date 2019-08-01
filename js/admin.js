1fe616c77aee6a082bb3d719275faf1a82429b51
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2019-08-01 15:18:43 +0200

    shorthand admin modul

--- js/admin.js
+++ js/admin.js
@@ -1,45 +1,17 @@
 //Checks
-
-exports.isUser = (arg) => {
-    return (arg && arg.match(/^<@!?([0-9]{18})>$/gm) !== null) ? true : false;
-}
-
-exports.isChannel = (arg) => {
-    return (arg && arg.match(/^<#!?([0-9]{18})>$/gm) !== null) ? true : false;   
-}
-
-exports.isAdmin = (message) => {
-    return message.member.permissions.has('ADMINISTRATOR');
-}
-
-exports.isMod = (message, config) => {
-    return [...config[0].Moderator].includes(message.author.id);
-}
-
-exports.isURL = (arg) => {
-    return (arg && arg.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm) !== null) ? true : false;
-}
-
-exports.isDigit = (arg) => {
-    return (arg && arg.match(/^[0-9]*$/gm) !== null) ? true : false;
-}
+exports.isUser = (arg) => (arg && arg.match(/^<@!?([0-9]{18})>$/gm) !== null);
+exports.isChannel = (arg) => (arg && arg.match(/^<#!?([0-9]{18})>$/gm) !== null);   
+exports.isAdmin = (message) => message.member.permissions.has('ADMINISTRATOR');
+exports.isMod = (message, config) =>  [...config[0].Moderator].includes(message.author.id);
+exports.isURL = (arg) => (arg && arg.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm) !== null);
+exports.isDigit = (arg) => (arg && arg.match(/^[0-9]*$/gm) !== null);
 
 //Gets
-
-exports.cut_cmd = async (msg) => {
+exports.cut_cmd = (msg) => {
     const args = msg.content.trim().split(/ +/g);
     args.shift();
     return args;
 }
-
-exports.get_message = async (client, channel_ID, message_ID) => {
-    return client.channels.get(channel_ID).fetchMessage(message_ID);
-}
-
-exports.get_channel = async (client, channel_ID) => {
-    return client.channels.get(channel_ID);
-}
-
-exports.to_colorcode = (color) => {
-    return parseInt(color, 16);
-}
+exports.get_message = (client, channel_ID, message_ID) => client.channels.get(channel_ID).fetchMessage(message_ID);
+exports.get_channel = (client, channel_ID) => client.channels.get(channel_ID);
+exports.to_colorcode = (color) => parseInt(color, 16);
