ba4e3fd50fb32971d5f0adea6507f64501c46e29
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2019-08-01 15:19:36 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-//Checks
+//check
 exports.isUser = (arg) => (arg && arg.match(/^<@!?([0-9]{18})>$/gm) !== null);
 exports.isChannel = (arg) => (arg && arg.match(/^<#!?([0-9]{18})>$/gm) !== null);   
 exports.isAdmin = (message) => message.member.permissions.has('ADMINISTRATOR');
@@ -6,7 +6,7 @@ exports.isMod = (message, config) =>  [...config[0].Moderator].includes(message.
 exports.isURL = (arg) => (arg && arg.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm) !== null);
 exports.isDigit = (arg) => (arg && arg.match(/^[0-9]*$/gm) !== null);
 
-//Gets
+//get
 exports.cut_cmd = (msg) => {
     const args = msg.content.trim().split(/ +/g);
     args.shift();
@@ -14,4 +14,6 @@ exports.cut_cmd = (msg) => {
 }
 exports.get_message = (client, channel_ID, message_ID) => client.channels.get(channel_ID).fetchMessage(message_ID);
 exports.get_channel = (client, channel_ID) => client.channels.get(channel_ID);
+
+//convert
 exports.to_colorcode = (color) => parseInt(color, 16);
