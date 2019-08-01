cb3bfbb1fcf1d1f2dd1b6874047bcb7f0c4720ec
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2019-08-01 14:58:31 +0200

    Update msg_send.js

--- js/msg_send.js
+++ js/msg_send.js
@@ -27,25 +27,3 @@ exports.embedimg = (client, channel_id, url) => {
     }
   });
 }
-
-
-/*
-exports.msg = () => {
-  const exampleEmbed = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Regular field title', 'Some value here')
-	.addBlankField()
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-  channel.send(exampleEmbed);
-}*/
