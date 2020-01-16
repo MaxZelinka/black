9de4b8f9f72d674a3a68b0339af022730c5539b8
LPGGNecromant <49428271+LPGGNecromant@users.noreply.github.com>
2020-01-16 11:12:14 +0100

    Create good_morning.js

---
+++ _module/good_morning.js
@@ -0,0 +1,10 @@
+/*
+Autor:          Necromant
+Date:           14.01.2020
+Description:    Say Good Morning every day (for the current time zone with an random giphy gif)
+*/
+
+/*init*/
+(function init() {
+
+}());
